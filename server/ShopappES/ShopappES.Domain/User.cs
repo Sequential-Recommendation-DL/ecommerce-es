aa9759897209e5a9bb65b0dc6b36d49ab0b1079d
dont-wait <nguyentansang15042005@gmail.com>
2026-04-12 21:24:27 +0700

    sync

--- server/ShopappES/ShopappES.Domain/User.cs
+++ server/ShopappES/ShopappES.Domain/User.cs
@@ -1,7 +1,11 @@
 namespace ShopappES.Domain
 {
-   public class User
-   {
-       
-   } 
+    public class User
+    {
+        public Guid UserId { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Phone { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+    }
 }
